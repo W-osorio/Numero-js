01151f985a720035ada6cf0718d20e947285b61c
W-osorio <111244407+W-osorio@users.noreply.github.com>
2023-07-05 19:20:31 -0400

    Add files via upload

---
+++ Numeros.js
@@ -0,0 +1,6 @@
+const alturaCm = 178;
+const alturaMetro = 1.78;
+const pesoKg = 80.4;
+const alturaRedondeada = alturaMetro.toFixed();
+const pesoRedondeo = Math.floor(pesoKg);
+const maximoValor = Number.MAX_VALUE + 1 === Number.MAX_VALUE;
